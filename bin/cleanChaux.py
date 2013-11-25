5021d83064a220266640e81286546e5f2899abcf
Glenn Hickey <hickey@soe.ucsc.edu>
2013-11-25 17:56:44 -0500

    add script to clean chaux files for training

---
+++ bin/cleanChaux.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+#Copyright (C) 2013 by Glenn Hickey
+#
+#Released under the MIT license, see LICENSE.txt
+import sys
+import os
+import argparse
+
+from pybedtools import BedTool, Interval
+
+"""
+Remove everything past the first | in the name column
+"""
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Cut names off at first |")
+    parser.add_argument("inBed", help="bed with ltr results to process")
+    parser.add_argument("outBed", help="bed to write output to.")
+
+    args = parser.parse_args()
+    assert os.path.exists(args.inBed)
+    outFile = open(args.outBed, "w")
+
+    for interval in BedTool(args.inBed).sort():
+        interval.name = interval.name[:interval.name.find("|")]
+        outFile.write(str(interval))
+
+    outFile.close()
+        
+if __name__ == "__main__":
+    sys.exit(main())
