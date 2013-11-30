ed6460face070eb3a10152a789bea04d491ada42
Glenn Hickey <hickey@soe.ucsc.edu>
2013-11-30 16:54:22 -0500

    overhaul cfg to relax CNF requirement which was getting annoying.  can now support a pair emission model, which is what needs to be done next

--- cfg.py
+++ cfg.py
@@ -35,111 +35,124 @@ these could be states corresponding to LTR or TSD).  All other states are
 treated as HMM states (ie emit one column at a time left-to-right.  For now
 these sets are disjoint, ie the nest states always emit pairs and the left to
 right don't though that could be relaxed in theory.  For now only the
-supervised training is supported, and we donn't implement full EM algorithm."""
+supervised training is supported, and we donn't implement full EM algorithm.
+
+All productions look like one of the following (nonterminals are capitals):
+
+X -> Y Z
+X -> a
+X -> aa
+X -> a Y a
+
+The first two are Chmosky Normal Form, but we add the last two kinds because
+we want to explicitly model pair emissions.
+"""
 class MultitrackCfg(object):
     def __init__(self, emissionModel=None,
                  nestStates=[]):
+        """ For now we take in an emission model (that was used for the HMM)
+        and a list singling out a few states as nested / pair emission states"""
         self.emissionModel = emissionModel
-        self.logProbs = None
+        self.logProbs1 = None
+        self.logProbs2 = None
         self.startProbs = None
 
         # all states that can emit a column
-        self.emittingStates = [x for x in xrange(emissionModel.getNumStates())]
+        self.M = self.emissionModel.getNumStates()
+        self.emittingStates = np.arange(self.M)
 
-        # states that can emit but are not hmm states.  we keep track of their
-        # id and index in a dictionary
-        self.nestStates = dict()
-        self.nestBack = dict()
-        for idx, state in enumerate(list(nestStates)):
-            self.nestStates[state] = idx
-            self.nestBack[idx] = state
-        
-        # all the LR states (HMM states)
+        # all states flagged as nest enabled (used by training)
+        self.nestStates = np.array(nestStates)
+
+        # all states that are LR only (ie the rest)
         self.hmmStates = []
         for i in self.emittingStates:
-            if i not in nestStates:
+            if i not in self.nestStates:
                 self.hmmStates.append(i)
-
-        # we need to add a pair of CFG states for each nest state
-        # for Chmosky Normal Form.
-        # In particular, the CNF states are constructed as followed for
-        # some nest State X (that emits x).
-        # CNF(X)_1 -> X CNF(X)_2 (100% of the time)
-        # CNF(X)_2 -> Y X (where Y is any HMM or CNF_1 state)
-        # (and X is only reachable as a RHS of the above two productions)
-        self.cnfStates = []
-        for i in self.nestStates:
-            self.cnfStates.append(self.getCnf(i))
-            assert self.getBase(self.getCnf(i)) == i
-            self.cnfStates.append(self.getCnf(i) + 1)
-            assert self.getBase(self.getCnf(i) + 1) == i
-            
-        # total number of states
-        self.M = len(self.hmmStates) + len(self.cnfStates) + \
-                 len(self.nestStates)
-
-        # sanity check to make sure we dont have same state twice
-        assert len(set(self.hmmStates).union(
-            set(self.cnfStates)).union(set(self.nestStates))) == self.M
-
+        self.hmmStates = np.array(self.hmmStates)
+        assert len(self.hmmStates) + len(self.nestStates) == self.M
+        
+        # start with a basic flat distribution
         self.initParams()
 
-    def getCnf(self, state):
-        """ Map an emitting nested state to its first CNF state"""
-        return len(self.emittingStates) + 2 * self.nestStates[state]
+    def createTables(self):
+        """ all probabailities of the form X -> Y Z (note that we will only
+        ever use a small subset.  will keep track using a shortcut table for
+        iterations """
+        self.logProbs1 = NEGINF + np.zeros((self.M, self.M, self.M),
+                                        dtype = np.float)
 
-    def getBase(self, cnf):
-        """ Map a CNF state to its corresponding emitting state """
-        cnfIdx = (cnf - len(self.emittingStates)) / 2
-        return self.nestBack[cnfIdx]
+        """ all probabilities of the form X -> aYa (ie Y is nested inside of X)
+        note that we are not counting probability of emitting a """
+        self.logProbs2 = NEGINF + np.zeros((self.M, self.M), dtype = np.float)
+
+    def createHelperTables(self):
+        """ to avoid iterating over all possible productions when many may
+        have zero probaiblities, we keep arrays of nozero productions """
+        self.helper1 = []
+        self.helperDim1 = np.zeros((self.M,), dtype=np.int32)
+        maxRow = 0
+        for state in self.emittingStates:
+            stateList = []
+            for next1 in self.emittingStates:
+                for next2 in self.emittingStates:
+                    if self.logProbs1[state, next1, next2] > NEGINF:
+                        stateList.append([next1, next2])
+            maxRow = max(len(stateList), maxRow)
+            self.helperDim1[state] = len(stateList)
+            self.helper1.append(stateList)
+        npHelper1 = -1 + np.zeros((self.M, maxRow, 2), dtype=np.float)
+        for state in xrange(len(self.helper1)):
+            for i, entry in enumerate(self.helper1[state]):
+                assert len(entry) == 2
+                npHelper1[state, i] = entry
+        self.helper1 = npHelper1
+
+        self.helper2 = []
+        self.helperDim2 = np.zeros((self.M,), dtype=np.int32)
+        maxRow = 0
+        for state in self.emittingStates:
+            stateList = []
+            for nextState in self.emittingStates:
+                if self.logProbs2[state, nextState] > NEGINF:
+                    stateList.append(nextState)
+            maxRow = max(len(stateList), maxRow)
+            self.helperDim2[state] = len(stateList)
+            self.helper2.append(stateList)
+        npHelper2 = -1 + np.zeros((self.M, maxRow), dtype=np.float)
+        for state in xrange(len(self.helper2)):
+            for i, entry in enumerate(self.helper2[state]):
+                npHelper2[state, i] = entry
+        self.helper2 = npHelper2
+        
 
     def initParams(self):
         """ Allocate the production (transition) probability matrix and
         initialize it to a flat distribution where everything has equal prob."""
-        self.startProbs = NEGINF + np.zeros((self.M,), dtype=np.float)
+        self.createTables()
+
+        # flat start probs over all states
+        oneOfAny = np.log(1. / self.M)
+        self.startProbs = oneOfAny + np.zeros((self.M,), dtype=np.float)
+
+        # hmm states flat of X - > X Y (where Y is any state)
         for state in self.hmmStates:
-            self.startProbs[state] = np.log(
-                    1. / (len(self.emittingStates)))
-        for i in xrange(0, len(self.cnfStates), 2):
-            cnf1 = self.cnfStates[i]
-            self.startProbs[cnf1] = np.log(
-                    1. / (len(self.emittingStates)))
+            for nextState in self.emittingStates:
+                self.logProbs1[state, state, nextState] = oneOfAny
+
+        # cfg states X -> a Y a (prob Y nested in X).  X is a nested state
+        # but Y is any state
+        # (-1 below to correct counting for state == nextState case)
+        oneOfCfgRHS = np.log(1. / (3.0 * self.M - 1.0))
+        for state in self.nestStates:
+            for nextState in self.emittingStates:
+                self.logProbs1[state, state, nextState] = oneOfCfgRHS
+                self.logProbs1[state, nextState, state] = oneOfCfgRHS
+                self.logProbs2[state, nextState] = oneOfCfgRHS
         
-        self.logProbs = NEGINF + \
-                        np.zeros((self.M, self.M, self.M), dtype=np.float)
-
-        # do the hmm style productions (X -> XY)
-        for state in self.hmmStates:
-            # we can always got left to right to an hmm state
-            for nextState in self.hmmStates:
-                self.logProbs[state, state, nextState] = np.log(
-                    1. / (len(self.emittingStates)))
-            # we can also go left to right to a CNF1 state (which essentially
-            # opens up a new nest pair)
-            for nextState in self.nestStates:
-                cnf1 = self.getCnf(nextState)
-                self.logProbs[state, state, cnf1] = np.log(
-                    1. / (len(self.emittingStates)))
-
-        # do the cfg stype productions (CNF_1 -> XCNF_2; CNF_2 -> YX)
-        assert len(self.cnfStates) % 2 == 0
-        for i in xrange(0, len(self.cnfStates), 2):
-            cnf1 = self.cnfStates[i]
-            cnf2 = self.cnfStates[i + 1]
-            base = self.getBase(cnf1)
-            assert base == self.getBase(cnf2)
-            # we can nest any hmm state
-            self.logProbs[cnf1, base, cnf2] = 0.
-            for nextState in self.hmmStates:
-                self.logProbs[cnf2, nextState, base] = np.log(
-                    1. / (len(self.emittingStates)))
-            # we can also nest a CNF1 state (which essentially
-            # opens up a new nest pair)
-            for nextState in self.nestStates:
-                nextCnf1 = self.getCnf(nextState)
-                self.logProbs[cnf2, nextCnf1, base] = np.log(
-                    1. / (len(self.emittingStates)))
-
+        # create shortcut tables:
+        self.createHelperTables()
+       
         self.validate()
 
     def validate(self):
@@ -147,31 +160,42 @@ class MultitrackCfg(object):
         up to one"""
         
         total = 0.
-        for state in xrange(self.M):
+        for state in self.emittingStates:
             total += np.exp(self.startProbs[state])
         assert_array_almost_equal(total, 1.)
         
-        for origin in xrange(self.M):
+        for origin in self.emittingStates:
             total = 0.
-            for next1 in xrange(self.M):
-                for next2 in xrange(self.M):
-                    total += np.exp(self.logProbs[origin, next1, next2])
-            if origin in self.nestStates:
-                assert_array_almost_equal(total, 0.)
-            else:
-                assert_array_almost_equal(total, 1.)
+            for next1 in self.emittingStates:
+                for next2 in self.emittingStates:
+                    total += np.exp(self.logProbs1[origin, next1, next2])
+                total += np.exp(self.logProbs2[origin, next1])
+            assert_array_almost_equal(total, 1.)
+
+        # now verify the helper tables exhibit the same behaviour
+        for origin in self.emittingStates:
+            total = 0.
+            for i in xrange(self.helperDim1[origin]):
+                total += np.exp(self.logProbs1[origin,
+                                               self.helper1[origin, i][0],
+                                               self.helper1[origin, i][1]])
+            for i in xrange(self.helperDim2[origin]):
+                total += np.exp(self.logProbs2[origin,
+                                               self.helper2[origin, i]])
+            assert_array_almost_equal(total, 1.)                
 
     def __initDPTable(self, obs):
         """ Create the 2D dynamic programming table for CYK etc. and initialise
         all the 1-length entries for each (emitting) state"""
-        self.table = NEGINF + np.zeros((len(obs), len(obs), self.M),
+        self.dp = NEGINF + np.zeros((len(obs), len(obs), self.M),
                                       dtype = np.float)
         emLogProbs = self.emissionModel.allLogProbs(obs)
         # todo: how fast is this type of loop?
         assert len(emLogProbs) == len(obs)
         for i in xrange(len(obs)):
             for j in xrange(len(emLogProbs[i])):
-                self.table[i,i,j] = emLogProbs[i,j]
+                self.dp[i,i,j] = emLogProbs[i,j]
+        # todo: init 'aa' states from pair model!!
 
     def __initTraceBackTable(self, obs):
         """ Create a dynamic programming traceback (for CYK) table to remember
@@ -188,30 +212,38 @@ class MultitrackCfg(object):
         for size in xrange(2, len(obs) + 1):
             for i in xrange(len(obs) + 1 - size):
                 j = i + size - 1
-                for k in xrange(i, i + size - 1):
-                    # test that X_ij -> Yi_k Zk+1_j (everything inclusive)
-                    # TODO: optimize loops for hmm vs cfg states the brute
-                    # for below is extremely wasteful (O(M^3)) insead of
-                    # O(grammar size)
-                    for lState in xrange(self.M):
-                        for r1State in xrange(self.M):
-                            for r2State in xrange(self.M):
-                                lp = self.logProbs[lState, r1State, r2State] + \
-                                     self.table[i, k, r1State] +\
-                                     self.table[k+1, j, r2State]
-                                if lp > self.table[i, j, lState]:
-                                    self.table[i, j, lState] = lp
-                                    self.tb[i, j, lState] = [k, r1State, r2State]
+                for lState in self.emittingStates:
+                    for q in xrange(len(self.helperDim1)):
+                        r1State = self.helper1[lState, q, 0]
+                        r2State = self.helper1[lState, q, 1]
+                        for k in xrange(i, i + size - 1):
+                            lp = self.logProbs1[lState, r1State, r2State] + \
+                                     self.dp[i, k, r1State] +\
+                                     self.dp[k+1, j, r2State]
+                            if lp > self.dp[i, j, lState]:
+                                self.dp[i, j, lState] = lp
+                                self.tb[i, j, lState] = [k, r1State, r2State]
+                    if size > 2 and i > 0 and j < (len(obs) - 1):
+                        for q in xrange(len(self.helperDim2)):
+                            rState = self.helper2[lState, q]
+                            ##### TODO: Pair emission  ###########
+                            lp = self.logProbs2[lState, rState] +\
+                                 self.dp[i+1, j-1, rState] +\
+                                 self.dp[i, i, lState] +\
+                                 self.dp[j, j, lState]
+                            if lp > self.dp[i, j, lState]:
+                                self.dp[i, j, lState] = lp
+                                self.tb[i, j, lState] = [k, r1State, r2State]
 
-        score = max([self.startProbs[i] + self.table[0, len(obs)-1, i]  \
-                     for i in xrange(self.M)])
+        score = max([self.startProbs[i] + self.dp[0, len(obs)-1, i]  \
+                     for i in self.emittingStates])
         return score
 
     def __traceBack(self, obs):
         """ depth first search to determine most likely states from the
         traceback table (self.tb) that was constructed during __cyk"""
-        trace = -1 + np.zeros(len(self.table))
-        top = np.argmax([self.startProbs[i] + self.table[0, len(obs)-1, i]\
+        trace = -1 + np.zeros(len(self.dp))
+        top = np.argmax([self.startProbs[i] + self.dp[0, len(obs)-1, i]\
                              for i in xrange(self.M)])
         self.assigned = 0
         def tbRecursive(i, j, state, trace):
