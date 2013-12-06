4580041e8c0c593809a4156d69905a73cd89ff59
Glenn Hickey <hickey@soe.ucsc.edu>
2013-12-06 10:19:30 -0500

    change bin scripts to accept both cfg and hmm.  also change pickle interface to make this easier.  note that cfg still missing implementations of key methods

---
+++ modelIO.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+#Copyright (C) 2013 by Glenn Hickey
+#
+# Class derived from _BaseHMM and MultinomialHMM from sklearn/tests/hmm.py
+# (2010 - 2013, scikit-learn developers (BSD License))
+#
+#Released under the MIT license, see LICENSE.txt
+#!/usr/bin/env python
+
+import os
+import sys
+import numpy as np
+import pickle
+import logging
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+from .hmm import MultitrackHmm
+from .cfg import MultitrackCfg
+
+def loadModel(path):
+    """ load an hmm or cfg from a pickle file """
+    f = open(path, "rb")
+    model = pickle.load(f)
+    f.close()
+    assert isinstance(model, MultitrackCfg) or isinstance(model, MultitrackHmm)
+    model.validate()
+    return model
+
+def saveModel(path, model):
+    """ save an hmm or cfg to a pickle file """
+    assert isinstance(model, MultitrackCfg) or isinstance(model, MultitrackHmm)
+    model.validate()  
+    f = open(path, "wb")
+    pickle.dump(model, f, 2)
+    f.close()
+
