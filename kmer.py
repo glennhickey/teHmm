1639df7b52310b68762f6b3dbcf86305d859f09a
Glenn Hickey <glenn.hickey@gmail.com>
2014-01-14 19:23:59 -0500

    start work on little short match detector thats still a ways from being optimal

---
+++ kmer.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+#Copyright (C) 2013 by Glenn Hickey
+#
+#Released under the MIT license, see LICENSE.txt
+#!/usr/bin/env python
+
+import os
+import sys
+import logging
+import numpy as np
+import xml.etree.ElementTree as ET
+
+
+class KmerTable:
+    """ Hash table of kmers for quick seed alignment.  Easier to 
+    just reimplement here for now than go through some big library
+    that's tuned to look for longer matches etc..  Each string is 
+    mapped to a list (which should be kept sorted) of positions"""
+    def __init__(self, kmerLen = 3, hashFn = None):
+        if hashFn is None:
+            # very inefficent space-wise: for testing purposes only
+            self.hashFn = lambda x: x
+        else:
+            self.hashFn = hashFn
+        self.table = dict()
+        self.kmerLen = kmerLen
+
+    def addKmer(self, kmer, position):
+        """ insert position into the kmer table."""
+        assert len(kmer) == self.kmerLen
+        key = self.hashFn(kmer)
+        if key not in self.table:
+            self.table[key] = [position]
+        else:
+            assert self.table[key][-1] <= position
+            self.table[key].append(position)
+
+    def getKmer(self, kmer):
+        """ return list of positions of kmer in table."""
+        assert len(kmer) == self.kmerLen
+        key = self.hashFn(kmer)
+        if key not in self.table:
+            return []
+        else:
+            return self.table[key]
+
+    def loadString(self, targetText):
+        """ create a kmer hash from given string """
+        self.table = dict()
+        for i in xrange(max(0, len(targetText) - self.kmerLen)):
+            self.addKmer(targetText[i:i+self.kmerLen], i)
+
+    def exactMatches(self, queryText, minMatchLen = 3, maxMatchLen = 10):
+        """ find all words in the table of length matchLength that
+        are exactly present in queryText.  Each patch is a pair (i,j)
+        where i is the position of the character of the match in 
+        queryText, and j is the first position in the table (corpus)"""
+        assert minMatchLen >= self.kmerLen
+        N = len(queryText)
+        closedMatches = []
+        activeMatches = []
+        for i in xrange(max(0, N - self.kmerLen + 1)):
+            key = self.hashFn(queryText[i:i+self.kmerLen])
+            if key in self.table:
+                matchPositions = self.table[key]
+                matchPairs = [[i, i+self.kmerLen, j, j+ self.kmerLen] 
+                              for j in matchPositions]
+                activeMatches = self.resolveOverlaps(matchPairs, 
+                                                     activeMatches, 
+                                                     closedMatches,
+                                                     maxMatchLen)
+        return closedMatches + activeMatches
+
+    def resolveOverlaps(self, matches, activeMatches, closedMatches, 
+                        maxMatchLen):
+        """merge pairwise alignments in matches list that overlap with pairs in 
+        activeMatches.   any matches that 
+        have no hope of being merged in the future (during the left-right scan)
+        get added to closedMatches"""
+        assert len(matches) > 0
+        if len(activeMatches) == 0:
+            return matches
+        
+        outList = []
+
+        for i in xrange(len(matches)):
+            found = False
+            for j in xrange(len(activeMatches)):
+                mergedMatch = None
+                if activeMatches[j][1] - activeMatches[j][0] + \
+                  matches[i][1] - matches[i][0] < maxMatchLen:
+                    mergedMatch = self.getMerge(activeMatches[j], matches[i])
+                if mergedMatch is not None:
+                    activeMatches[j] = mergedMatch
+                    found = True
+                    break
+            if not found:
+                outList.append(matches[i])
+
+        # todo: use closedMatches
+        return activeMatches + outList
+
+    def getMerge(self, leftMatch, rightMatch):
+        """ return the merge of two pairwise alignments, by extending the left one
+        by the length of the overhand.  if no valid merge possible, return None"""        
+        assert leftMatch[0] <= rightMatch[0]
+        if rightMatch[0] >= leftMatch[0] and rightMatch[0] <= leftMatch[1] and\
+          rightMatch[2] >= leftMatch[2] and rightMatch[2] <= leftMatch[3] and\
+          rightMatch[1] >= leftMatch[1]:
+            ovl1 = rightMatch[1] - leftMatch[1]
+            ovl2 = rightMatch[3] - leftMatch[3]
+            if ovl1 == ovl2:
+              return [leftMatch[0], leftMatch[1] + ovl1, leftMatch[2], leftMatch[3] + ovl1]
+        return None
+          
+             
+        
+                
---
+++ tests/kmerTest.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+#Copyright (C) 2013 by Glenn Hickey
+#
+# Contains some fragments of code from sklearn/tests/test_hmm.py
+# (2010 - 2013, scikit-learn developers (BSD License))
+#
+#Released under the MIT license, see LICENSE.txt
+import unittest
+import sys
+import os
+import math
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+from teHmm.kmer import KmerTable
+
+from teHmm.tests.common import getTestDirPath
+from teHmm.tests.common import TestBase
+from teHmm.tests.bedTrackTest import getTracksInfoPath
+from teHmm.tests.emissionTest import getBedStates
+
+
+class TestCase(TestBase):
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+    
+    def tearDown(self):
+        super(TestCase, self).tearDown()
+
+    def testLoad(self):
+        s = "hey hey we're the monkees"
+        kt = KmerTable()
+        kt.loadString(s)
+        assert kt.getKmer("hey") == [0, 4]
+        assert kt.getKmer("y h") == [2]
+        assert kt.getKmer("mon") == [18]
+        assert kt.getKmer("key") == []        
+
+    def testOverlap(self):
+        kt = KmerTable()
+        assert kt.getMerge([0, 4, 10, 14], [1, 5, 11, 15]) == [0, 5, 10, 15]
+        assert kt.getMerge([0, 4, 10, 14], [1, 5, 12, 16]) == None
+        assert kt.getMerge([9, 12, 3, 6], [10, 13, 4, 7]) == [9, 13, 3, 7]
+
+    def testMatch(self):
+        s = "hey hey we're the monkees"
+        kt = KmerTable()
+        kt.loadString(s)
+        em = kt.exactMatches("monkees")
+        assert em == [[0, 6, 18, 24]]
+        em = kt.exactMatches("something hey")
+        assert len(em) == 2
+        assert [9,13,3,7] in em
+        assert [10,13,0,3] in em
+
+def main():
+    sys.argv = sys.argv[:1]
+    unittest.main()
+        
+if __name__ == '__main__':
+    main()
+
