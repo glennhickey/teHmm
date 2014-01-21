6a08054d7882849999fb6e1f43ba38b887cd73bb
Glenn Hickey <glenn.hickey@gmail.com>
2014-01-21 15:39:42 -0500

    extend to work on output of cleantermini

--- bin/fillTermini.py
+++ bin/fillTermini.py
@@ -24,6 +24,8 @@ becomes
 scaffold_1	141	225	1+	43	+
 scaffold_1	225	4479	1+	43	+
 scaffold_1	4479	4563	1+	43	+
+
+Note: also works on output of cleanTermini.
 """
 
 def main(argv=None):
@@ -33,7 +35,8 @@ def main(argv=None):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="Add interval between pairs of candidate termini.  Input "
-    "bed must have pairs of termini (left first) in contiguous rows")
+    "bed must have pairs of termini (left first) in contiguous rows (or be "
+    "like output of cleanTermini.py")
     parser.add_argument("inBed", help="bed with ltr results to process")
     parser.add_argument("outBed", help="bed to write output to.")
     
@@ -46,7 +49,8 @@ def main(argv=None):
         
         # Right termini
         if prevInterval is not None:
-            if interval.name != prevInterval.name:
+            if interval.name != prevInterval.name and (
+                    interval.name != "R_term" or prevInterval.name != "L_term"):
                 raise RuntimeError("Consecutive intervals dont have same id"
                                    "\n%s%s" % (prevInterval, interval))
 
