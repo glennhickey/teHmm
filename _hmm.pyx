6ee60e252505d6100c227486c1eb11de2161c092
Glenn Hickey <glenn.hickey@gmail.com>
2014-02-21 12:51:29 -0500

    reimplement cython viterbi

--- _hmm.pyx
+++ _hmm.pyx
@@ -145,27 +145,40 @@ def _viterbi(int n_observations, int n_components,
     cdef int t, max_pos
     cdef np.ndarray[dtype_t, ndim = 2] viterbi_lattice
     cdef np.ndarray[np.int_t, ndim = 1] state_sequence
+    cdef np.ndarray[np.int16_t, ndim = 2] trace_back
     cdef dtype_t logprob
-    cdef np.ndarray[dtype_t, ndim = 2] work_buffer
+    cdef dtype_t maxprob
+    cdef dtype_t curprob
+    cdef np.int16_t maxState
 
     # Initialization
     state_sequence = np.empty(n_observations, dtype=np.int)
     viterbi_lattice = np.zeros((n_observations, n_components))
     viterbi_lattice[0] = log_startprob + framelogprob[0]
+    trace_back = np.empty((n_observations, n_components), dtype=np.int16)
 
     # Induction
-    for t in range(1, n_observations):
-        work_buffer = viterbi_lattice[t-1] + log_transmat.T
-        viterbi_lattice[t] = np.max(work_buffer, axis=1) + framelogprob[t]
-
+    for t in xrange(1, n_observations):
+        for toState in xrange(0, n_components):            
+            maxprob = viterbi_lattice[t-1, 0] + log_transmat[0, toState] +\
+              framelogprob[t, toState]
+            maxState = 0
+            for fromState in xrange(1, n_components):
+                curprob = viterbi_lattice[t-1, fromState] + \
+                  log_transmat[fromState, toState] +\
+                  framelogprob[t, toState]
+                if curprob > maxprob:
+                    maxprob = curprob
+                    maxState = fromState
+            viterbi_lattice[t, toState] = maxprob
+            trace_back[t, toState] = maxState
+            
     # Observation traceback
     max_pos = np.argmax(viterbi_lattice[n_observations - 1, :])
     state_sequence[n_observations - 1] = max_pos
     logprob = viterbi_lattice[n_observations - 1, max_pos]
 
-    for t in range(n_observations - 2, -1, -1):
-        max_pos = np.argmax(viterbi_lattice[t, :] \
-                + log_transmat[:, state_sequence[t + 1]])
-        state_sequence[t] = max_pos
+    for t in range(n_observations - 1, 0, -1):
+        state_sequence[t - 1] = trace_back[t, state_sequence[t]]
 
     return state_sequence, logprob
