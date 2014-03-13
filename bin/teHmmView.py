ac97b83be21ee76854b8310fb8ebe10746a82a73
Glenn Hickey <glenn.hickey@gmail.com>
2014-03-13 18:02:37 -0400

    start working on track distribution analysis

--- bin/teHmmView.py
+++ bin/teHmmView.py
@@ -7,11 +7,18 @@ import unittest
 import sys
 import os
 import argparse
+import numpy as np
 
 from teHmm.track import TrackData
 from teHmm.hmm import MultitrackHmm
 from teHmm.cfg import MultitrackCfg
 from teHmm.modelIO import loadModel
+try:
+    from teHmm.parameterAnalysis import plotHierarchicalClusters
+    from teHmm.parameterAnalysis import hierarchicalCluster
+    canPlot = True
+except:
+    canPlot = False
 
 def main(argv=None):
     if argv is None:
@@ -25,6 +32,8 @@ def main(argv=None):
                         " teHmmTrain.py")
     parser.add_argument("--nameMap", help="Print out name map tables",
                         action="store_true", default=False)
+    parser.add_argument("--ec", help="Print emission distribution clusterings"
+                        " to given file in PDF format", default=None)
     
     args = parser.parse_args()
 
@@ -44,6 +53,40 @@ def main(argv=None):
                 print "Track: %s" % track.getName()
                 print " map %s " % track.getValueMap().catMap
                 print " pam %s " % track.getValueMap().catMapBack
+
+    if args.ec is not None:
+        if canPlot is False:
+            raise RuntimeError("Unable to write plots.  Maybe matplotlib is "
+                               "not installed?")
+        writeEmissionClusters(model, args)
+
+def writeEmissionClusters(model, args):
+    """ print a hierachical clustering of states for each track (where each
+    state is a point represented by its distribution for that track)"""
+    trackList = model.getTrackList()
+    stateNameMap = model.getStateNameMap()
+    emission = model.getEmissionModel()
+    # [TRACK][STATE][SYMBOL]
+    emissionDist = np.exp(emission.getLogProbs())
+
+    # leaf names of our clusters are the states
+    stateNames = map(stateNameMap.getMapBack, xrange(len(stateNameMap)))
+    N = len(stateNames)
+
+    # cluster for each track
+    hcList = []
+    hcNames = []
+    for track in trackList:
+        hcNames.append(track.getName())
+        points = [emissionDist[track.getNumber()][x] for x in xrange(N)]
+        hc = hierarchicalCluster(points, normalizeDistances=True)
+        hcList.append(hc)
+
+    # write clusters to pdf
+    plotHierarchicalClusters(hcList, hcNames, stateNames, args.ec)
+        
+    #todo: ranking
+    
     
 if __name__ == "__main__":
     sys.exit(main())
---
+++ parameterAnalysis.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+#Copyright (C) 2014 by Glenn Hickey
+#
+#Released under the MIT license, see LICENSE.txt
+#!/usr/bin/env python
+
+import os
+import sys
+import logging
+import argparse
+import numpy as np
+import scipy
+import scipy.spatial
+import scipy.cluster
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.backends.backend_pdf as pltBack
+import matplotlib.lines as lines
+import matplotlib.patches as patches
+import matplotlib.pylab  as pylab
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter, LogLocator
+from matplotlib.ticker import LogFormatter
+import matplotlib.mlab as mlab
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from teHmm.basehmm import normalize
+
+"""
+Methods to analyse parameter distributions of any graphical model.  In particular
+we are interested in finding the tracks that provide most information when it
+comes to distinguishing between states.  Note that this information is only
+relevant in conjunction with some measure of accuracy.  IE one track can show
+amazing performance for state selection but if it results in wrong predictions
+then it's not very useful.
+"""
+
+def hierarchicalCluster(points, normalizeDistances=False):
+    """ Perform simple hiearchical clustering using euclidian distance"""
+    assert points is not None and len(points) > 0
+    distanceMatrix = scipy.spatial.distance.pdist(points, "euclidean")
+    if normalizeDistances is True:
+        distanceMatrix = normalize(distanceMatrix)
+    hc = scipy.cluster.hierarchy.linkage(distanceMatrix, method='complete')
+    return hc
+
+def rankHierarchies(hcList, rankStat):
+    """ produce a ranking of hierarchical clusterings using one of a variety
+    of statistics. Each element of hcList is the output of
+    hierarchichalCluster() """
+    
+    pass
+
+def plotHierarchicalClusters(hcList, titles, leafNames, outFile):
+    """ print out a bunch of dendrograms to a PDF file.  Each element of
+    hcList is the output of hierarchichalCluster()"""
+    cols = 3
+    rows = int(np.ceil(float(len(hcList)) / float(cols)))
+    width=10
+    height=5 * rows
+
+    pdf = pltBack.PdfPages(outFile)
+    fig = plt.figure(figsize=(width, height))
+    plt.clf()
+    for i, hc in enumerate(hcList):
+        plt.subplot(rows, cols, i)
+        dgram = scipy.cluster.hierarchy.dendrogram(
+            hc, color_threshold=1, labels=leafNames, show_leaf_counts=False)
+#            p=6,
+#            truncate_mode='lastp')
+        plt.title(titles[i])
+        plt.setp(plt.xticks()[1], rotation=45)
+    fig.tight_layout()
+    fig.savefig(pdf, format = 'pdf')
+    pdf.close()
+
+def pcaFlatten(points, outDim = 2):
+    """ flatten points to given dimensionality using PCA """
+    pass
+
+def plotPoints2d(points, label, outFile):
+    """ plot some points to a pdf file """
+    pass
+
+
+### Crappy sandbox for testing ###
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    args = parser.parse_args()
+
+    linkageMat = hierarchicalCluster([(0,1), (1,2), (10,10), (10, 15)],
+                                     normalizeDistances = True)
+    plotHierarchicalClusters([linkageMat, linkageMat, linkageMat], ["yon", "Title", "Blin"], ["A", "B", "C", "D"],
+                             "blin.pdf")
+
+if __name__ == "__main__":
+    sys.exit(main())
