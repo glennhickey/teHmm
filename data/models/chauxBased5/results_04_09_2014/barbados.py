3bdbd704b03d5ed6eece962954f807bf504d3d8e
Glenn Hickey <glenn.hickey@gmail.com>
2014-04-10 08:45:17 -0400

    add graph script for barb slides

--- data/models/chauxBased5/results_04_09_2014/barbados.py
+++ data/models/chauxBased5/results_04_09_2014/barbados.py
@@ -63,9 +63,38 @@ for i in xrange(len(rgbs)):
     rgbs[i] = list(rgbs[i])
     rgbs[i][3] = 0.7
 
-rgbs = [rgbs[0], rgbs[0], rgbs[5], rgbs[2], rgbs[2], rgbs[3], rgbs[3]]    
+prgbs = [rgbs[0], rgbs[0], rgbs[5], rgbs[2], rgbs[2], rgbs[3], rgbs[3]]    
 
 plotPoints2d(distList, titles, stateNames, outFile, xRange=(-0.1,1.1),
                  yRange=(-0.1, 1.2), ptSize=75, xLabel="Precision",
                  yLabel="Recall", cols=2, width=10, rowHeight=5,
-                 markerList = markerList, rgbs = rgbs)
+                 markerList = markerList, rgbs = prgbs)
+
+iPoints = []
+bPoints = []
+stateNames = []
+
+# from mcSegment1_flat_comp.txt
+iPoints.append((0.6666666666666666, 0.9333333333333333))
+stateNames.append("HMM LTR")
+
+iPoints.append((0.7647058823529411, 0.625))
+stateNames.append("HMM NON-LTR")
+
+# from combined_flat_comp.txt
+iPoints.append((0.17105263157894737, 0.8666666666666667))
+stateNames.append("3way Union LTR")
+
+iPoints.append((0.5048543689320388, 0.828125))
+stateNames.append("3way Union NON-LTR")
+
+prgbs = [rgbs[0], rgbs[0], rgbs[6], rgbs[6]]
+
+titles = ["Element-level Accuracy (th=0.8)"]
+distList = [ iPoints ]
+assert len(distList[0]) == 4
+
+plotPoints2d(distList, titles, stateNames, outFile + "com.pdf", xRange=(-0.1,1.1),
+                 yRange=(-0.1, 1.2), ptSize=75, xLabel="Precision",
+                 yLabel="Recall", cols=2, width=10, rowHeight=5,
+                 markerList = markerList, rgbs = prgbs)
